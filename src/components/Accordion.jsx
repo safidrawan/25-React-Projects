342201f6d40624f2585a00123b9003509d4b7e83
Mohammad Rafi Safidrawan <safidrawan@gmail.com>
2025-04-01 23:52:33 +0430

    multi selection mode logic changed

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -2,26 +2,25 @@ import React, { useState } from "react";
 import data from "./data.js";
 
 function Accordion() {
-  const [isActive, setIsActive] = useState(null);
-  const [areActive, setAreActive] = useState([])
-  const [isMultiSelectionOn, setIsMultiSelectionOn] = useState(false)
+  const [activeItems, setActiveItems] = useState([]);
+  const [isMultiSelectionOn, setIsMultiSelectionOn] = useState(false);
 
   function handleActive(id) {
-    if (isMultiSelectionOn){
-     areActive.includes(id) ? setAreActive(prev=>{
-      return prev.filter(value => !value===id)
-     }) :  setAreActive(prev=>{
-      return [...prev,id]
-    })
+    if (isMultiSelectionOn) {
+      activeItems.includes(id)
+        ? setActiveItems((prev) => {
+            return prev.filter((value) => value !== id);
+          })
+        : setActiveItems((prev) => {
+            return [...prev, id];
+          });
     } else {
-      isActive === id ? setIsActive(null) : setIsActive(id);
+      activeItems.includes(id) ? setActiveItems([]) : setActiveItems([id]);
     }
   }
 
-  function toggleMultiSelection(){
-    setIsMultiSelectionOn(!isMultiSelectionOn)
-    setAreActive([])
-    setIsActive(null)
+  function toggleMultiSelection() {
+    setIsMultiSelectionOn(!isMultiSelectionOn);
   }
 
   const elements = data.map((item) => {
@@ -37,28 +36,36 @@ function Accordion() {
           <span
             className={`absolute top-1 right-0 transform transition-transform duration-
               400 ${
-              isActive === item.id ? "rotate-180" : ""
-            }`}
+                activeItems.includes(item.id) ? "rotate-180"
+                  : ""
+              }`}
           >
             ▼
           </span>
         </button>
         <div
           className={`text-lg rounded-b-lg flex justify-between w-full overflow-hidden transition-all duration-400 ${
-           areActive.includes(item.id) || isActive === item.id ?"max-h-96" : "max-h-0"
+            activeItems.includes(item.id) ? "max-h-96"
+              : "max-h-0"
           } `}
         >
-
           {item.content}
         </div>
       </div>
     );
   });
 
-  return <div className="max-w-5xl mx-auto">
-    <button onClick={toggleMultiSelection} className="text-2xl bg-blue-500 px-5 py-3 rounded-lg m-4 text-white cursor-pointer">Multi Selection Mode: {isMultiSelectionOn? "Yes":"No"}</button>
-    {elements}
-  </div>;
+  return (
+    <div className="max-w-5xl mx-auto">
+      <button
+        onClick={toggleMultiSelection}
+        className="text-2xl bg-blue-500 px-5 py-3 rounded-lg m-4 text-white cursor-pointer"
+      >
+        Multi Selection Mode: {isMultiSelectionOn ? "Yes" : "No"}
+      </button>
+      {elements}
+    </div>
+  );
 }
 
 export default Accordion;
