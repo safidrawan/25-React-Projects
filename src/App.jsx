82f022f3f9a4cfc5a1f157a74e5d49878c112843
Mohammad Rafi Safidrawan <safidrawan@gmail.com>
2025-04-02 16:03:16 +0430

    Star Rating initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Accordion from "./components/accordion/Accordion";
 import RandomColor from "./components/random-color/RandomColor";
+import StarRating from "./components/star-rating/StarRating";
 
 
 function App() {
   return <>
   <Accordion />
   <RandomColor /> 
+  <StarRating />
   </>;
 }
 
