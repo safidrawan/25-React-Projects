43bd7ac781d162cc5212208bf5942be62940847b
Mohammad Rafi Safidrawan <safidrawan@gmail.com>
2025-04-02 19:40:48 +0430

    Star rating done

--- src/components/star-rating/StarRating.jsx
+++ src/components/star-rating/StarRating.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useState } from "react";
+import { FaStar } from "react-icons/fa";
 
-function StarRating() {
+function StarRating({ noOfStars = 5 }) {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+
+  function handleClick(currentIndex) {
+    setRating(currentIndex);
+  }
+
+  function handleMouseEnter(currentIndex) {
+    setHover(currentIndex);
+  }
+
+  function handleMouseLeave(currentIndex) {
+    setHover(rating);
+  }
+
   return (
-    <div>StarRating</div>
-  )
+    <div className="star-rating flex h-40 justify-center items-center text-5xl">
+      {[...Array(noOfStars)].map((_, index) => {
+        index += 1;
+        return (
+          <FaStar
+            key={index}
+            className={index <= (hover||rating) ? "active":""}
+            onClick={() => {
+              handleClick(index);
+            }}
+            onMouseEnter={() => {
+              handleMouseEnter(index);
+            }}
+            onMouseLeave={() => {
+              handleMouseLeave();
+            }}
+          />
+        );
+      })}
+    </div>
+  );
 }
 
-export default StarRating
+export default StarRating;
